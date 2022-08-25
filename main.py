0dcb37c328c831bf4ea488f3894490237a058330
Daniel Davis <dda49@uclive.ac.nz>
2022-08-25 22:49:57 +1200

    More documentation

--- main.py
+++ main.py
@@ -86,7 +86,8 @@ class SolverBase(abc.ABC):
         """A method to simulate playing a particular answer.
 
         Args:
-            answer: The answer to try and find. If None is supplied, user input will be requested instead
+            answer: The answer to try and find. If None is supplied, user input will be
+                requested instead
             verbose: Whether to print information on the steps taken for a user
 
         Returns: An int representing the steps taken to find the answer
@@ -123,12 +124,23 @@ class SolverBase(abc.ABC):
 
         return results
 
-    def get_clean_input(self, best_word, info):
+    def get_clean_input(self, best_word: str, info: dict) -> str:
+        """Gets input from user and ensures it is a valid format
+        
+        Args:
+            best_word: The word for the solver to suggest to the user
+            info: A dictionary of information about the game state
+
+        Returns: A string representing hints or a command
+
+        """
+
         def is_clean(input_string):
             return len(input_string) == 5 and all(c in self.HINT_TYPES for c in input_string)
 
         user_input = input(
-            f"{info['available_target']:>4}|{info['available_words']:>5}: Use '{best_word}' and enter hints: ").lower()
+            f"{info['available_targets']:>4}|{info['available_words']:>5}: "
+            f"Use '{best_word}' and enter hints: ").lower()
         while not is_clean(user_input) and user_input != 'quit':
             user_input = input(f"Enter a 5 letter combination of {self.HINT_TYPES[0]},"
                                f" {self.HINT_TYPES[1]} and {self.HINT_TYPES[2]}: ").lower()
@@ -136,37 +148,61 @@ class SolverBase(abc.ABC):
         return user_input
 
     @abc.abstractmethod
-    def simulate_hints(self, guesses: str | np.ndarray, answer: str):
+    def simulate_hints(self, guesses: str | np.ndarray, answer: str) -> str | np.ndarray:
+        """Simulate the hints for a guess or guesses against a particular answer.
+        
+        Args:
+            guesses: The guess(es) to find hint(s) for
+            answer: The answer to check against
+
+        Returns: A string or array (matching the type of guesses) of hints
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
     def reset_state(self) -> None:
+        """Reset the solver's game state to its initial state
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
-    def update_state(self, word, hints) -> None:
+    def update_state(self, guess: str, hints: str) -> None:
+        """Iterate the solver's game state based on a guess and its corresponding hints
+        
+        Args:
+            guess: The most recent guess made in a game
+            hints: The hints obtained from the guess
+        """
         raise NotImplementedError
 
     def get_best_word(self) -> tuple[str, dict]:
+        """Based on the current game state, find the best next guess
+        
+        Returns: The solver's recommended next guess and a dictionary of information
+            about the guess (the number of remaining guesses and answers)
+        """
         info_dict = {
             'only_word': True,
             'available_words': -1,
-            'available_target': len(self.available_targets)
+            'available_targets': len(self.available_targets)
         }
         if len(self.available_targets) == 1:
             return self.available_targets[0], info_dict
 
-        available_words = self.available_word
         info_dict['only_word'] = False
-        info_dict['available_words'] = len(available_words)
+        info_dict['available_words'] = len(self.available_words)
         best_word = self._choose_best_word()
         return best_word, info_dict
 
     def _choose_best_word(self):
+        """The overwrite-able method for determining the next best guess
+        
+        Returns: The solver's recommended next guess
+        """
         return self.available_targets[0]
 
 
-class SolverMinTargets(SolverBase):
+class SolverMinTargets(SolverBase, abc.ABC):
 
     def __init__(self, **kwargs):
         self.word_matrix = None
@@ -189,8 +225,7 @@ class SolverMinTargets(SolverBase):
     def simulate_hints(self, guesses: str | np.ndarray, answer: str):
         if self.word_matrix is None:
             return super().simulate_hints(guesses, answer)
-        else:
-            return self.word_matrix.loc[guesses, answer]
+        return self.word_matrix.loc[guesses, answer]
 
     def _choose_best_word(self):
         reduced_word_matrix = self.word_matrix.loc[:, self.available_targets]
@@ -235,7 +270,7 @@ class PeaksSolverBase(SolverBase):
     HINT_TYPES = (BEFORE, AFTER, CORRECT)
 
     @property
-    def available_target(self):
+    def available_targets(self):
         split_guesses = self.available_targets.view('U1').reshape((len(self.available_targets), -1))
         mask = np.logical_and((self.game_state[0] <= split_guesses).all(axis=1),
                               (split_guesses <= self.game_state[1]).all(axis=1))
@@ -279,8 +314,8 @@ class PeaksSolverBase(SolverBase):
             ['z'] * 5
         ])
 
-    def update_state(self, word, hints):
-        split_word = np.array([word]).view('U1')
+    def update_state(self, guess, hints):
+        split_word = np.array([guess]).view('U1')
         split_hints = np.array([hints]).view('U1')
 
         tmp = np.array([
@@ -294,14 +329,18 @@ class PeaksSolverBase(SolverBase):
         tmp[1, split_hints == self.AFTER] = (split_word[split_hints == self.AFTER]
                                              .view('int') - 1).view('U1')
 
-        self.game_state[0, :] = np.maximum(self.game_state.view('int')[0, :], tmp.view('int')[0, :]).view('U1')
-        self.game_state[1, :] = np.minimum(self.game_state.view('int')[1, :], tmp.view('int')[1, :]).view('U1')
+        self.game_state[0, :] = (np.maximum(self.game_state.view('int')[0, :],
+                                            tmp.view('int')[0, :])
+                                 .view('U1'))
+        self.game_state[1, :] = (np.minimum(self.game_state.view('int')[1, :],
+                                            tmp.view('int')[1, :])
+                                 .view('U1'))
 
     def get_best_word(self):
         info_dict = {
             'only_word': True,
             'available_words': -1,
-            'available_target': len(self.available_targets)
+            'available_targets': len(self.available_targets)
         }
         if len(self.available_targets) == 1:
             return self.available_targets[0], info_dict
@@ -342,7 +381,7 @@ class WordleSolverBase(SolverBase):
         super().init()
 
     @property
-    def available_target(self):
+    def available_targets(self):
         target_letter_counts = self.letter_counts.loc[self.available_targets, :]
         min_count, max_count = (self.game_state.loc[target_letter_counts.letter, 'min'],
                                 self.game_state.loc[target_letter_counts.letter, 'max'])
@@ -395,36 +434,6 @@ class WordleSolverBase(SolverBase):
         GUESS_PLACEHOLDER = ','
         ANSWER_PLACEHOLDER = '.'
 
-        split_guess = np.array([guesses]).view('U1').reshape(-1)
-        split_answer = np.array([answer]).view('U1').reshape(-1)
-
-        hints_temp = np.full_like(split_guess, self.INCORRECT)
-
-        # Mark correct letters as such and then remove them from the options
-        correct_mask = split_guess == split_answer
-        hints_temp[correct_mask] = self.CORRECT
-        split_guess[correct_mask] = GUESS_PLACEHOLDER
-        split_answer[correct_mask] = ANSWER_PLACEHOLDER
-
-        answer_letter_counts = dict(zip(*np.unique(split_answer, return_counts=True)))
-
-        for i in range(5):
-            letter = split_guess[i]
-            if answer_letter_counts.get(letter, 0):
-                answer_letter_counts[letter] -= 1
-            else:
-                split_guess[i] = GUESS_PLACEHOLDER
-        hints_temp[split_guess != GUESS_PLACEHOLDER] = self.PARTIAL
-
-        # answer_matrix = np.broadcast_to(split_answer, (5, 5)).transpose()
-        # hints_temp[(answer_matrix == split_guess).any(axis=0)] = self.PARTIAL
-
-        return hints_temp.view('U5')[0]
-
-    def simulate_hints_(self, guesses: str | np.ndarray, answer: str):
-        GUESS_PLACEHOLDER = ','
-        ANSWER_PLACEHOLDER = '.'
-
         split_guesses = np.array([guesses]).view('U1').reshape((-1, 5))
         split_answer = np.array([answer]).view('U1')
         split_answer_pd = pd.DataFrame(np.broadcast_to(split_answer, split_guesses.shape),
@@ -467,8 +476,8 @@ class WordleSolverBase(SolverBase):
         self.game_state['min'] = 0
         self.game_state['max'] = 5
 
-    def update_state(self, word, hints):
-        split_combo = pd.DataFrame(zip(word, hints), columns=['letter', 'hint'])
+    def update_state(self, guess, hints):
+        split_combo = pd.DataFrame(zip(guess, hints), columns=['letter', 'hint'])
         split_info = (split_combo
                       .value_counts(sort=False)
                       .reset_index(level='hint')
@@ -595,9 +604,7 @@ Usage:
         pass
 
 
-if __name__ == '__main__':
-    # shell = SolverCmd()
-    # shell.cmdloop()
+def test():
     profiler = cProfile.Profile(subcalls=False, builtins=False)
     profiler.enable()
     solver = WordleSolverMinTargets(verbose=True)
@@ -612,3 +619,9 @@ if __name__ == '__main__':
     print(f"The average score for {type(solver).__name__} is {average_score:.4f}")
     print(distribution)
     print('Worst words:', results.loc[results == distribution.index.max()].index.to_list())
+
+
+if __name__ == '__main__':
+    # shell = SolverCmd()
+    # shell.cmdloop()
+    test()
